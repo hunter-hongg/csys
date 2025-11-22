dcc042f7dc3c1cfa074e1e66c7e0650580926493
hunter-hongg <2843249813@qq.com>
2025-11-22 11:32:00 +0800

    基本完成开发

--- include/csys.h
+++ include/csys.h
@@ -10,12 +10,16 @@
 extern "C" {
 #endif
 
-CError CSys_file_new                   (const char*);
-CError CSys_file_delete                (const char*);
-void   CSys_file_chmod                 (const char*, mode_t);
-CError CSys_dir_new                    (const char*);
-CError CSys_dir_delete                 (const char*);
-void   CSys_dir_chmod                  (const char*, mode_t);
+CError        CSys_file_new                   (const char*);           // 新建文件
+CError        CSys_file_delete                (const char*);           // 删除文件
+void          CSys_file_chmod                 (const char*, mode_t);   // 更改文件权限
+struct stat*  CSys_file_stat                  (const char*);           // 获取文件权限
+CError        CSys_dir_new                    (const char*);           // 新建空目录
+CError        CSys_dir_delete                 (const char*);           // 删除空目录
+void          CSys_dir_chmod                  (const char*, mode_t);   // 更改目录权限
+struct stat*  CSys_dir_stat                   (const char*);           // 获取目录权限
+
+#define CSys_INCLUDE &
 
 #ifdef __cplusplus
 }
--- src/csys.c
+++ src/csys.c
@@ -1,4 +1,4 @@
-#include "cerror.h"
+#include "glib.h"
 #include <csys.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -22,4 +22,33 @@ CError CSys_file_delete(const char *filename) {
 }
 void CSys_file_chmod(const char *filename, mode_t mode) {
   chmod(filename, mode);
+  return;
+}
+struct stat *CSys_file_stat(const char *filename) {
+  struct stat *buffer = (struct stat *)(g_malloc(sizeof(struct stat)));
+  stat(filename, buffer);
+  return buffer;
+}
+CError CSys_dir_new(const char *dirname) {
+  int rtv = mkdir(dirname, 0755);
+  if (rtv == 0) {
+    return CErrorNew(1, "");
+  } else {
+    return CErrorNew(0, "创建目录出错");
+  }
+}
+CError CSys_dir_delete(const char *dirname) {
+  int rtv = rmdir(dirname);
+  if (rtv == 0) {
+    return CErrorNew(1, "");
+  } else {
+    return CErrorNew(0, "删除目录出错");
+  }
+}
+void CSys_dir_chmod(const char *dirname, mode_t mode) {
+  chmod(dirname, mode);
+  return;
+}
+struct stat *CSys_dir_stat(const char *dirname) {
+  return CSys_file_stat(dirname);
 }
